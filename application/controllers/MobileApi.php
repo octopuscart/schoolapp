f27ea28774653fa10aa43bdaf0eb82cf4f2ae168
dev <dev@develop.com>
2019-10-07 22:04:33 +0530

    changes

--- application/controllers/MobileApi.php
+++ application/controllers/MobileApi.php
@@ -160,7 +160,7 @@ class MobileApi extends REST_Controller {
 
     function uploadFileImage_post() {
         $this->config->load('rest', TRUE);
-        $this->response(array("last_id" => "sdsd"));
+        $this->response(array("last_id" => $_FILES));
         
     }
 

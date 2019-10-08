094ac8e4823a58ba978f134691fe6e37efd1798d
dev <dev@develop.com>
2019-10-08 13:57:29 +0530

    changes

--- application/config/rest.php
+++ application/config/rest.php
@@ -29,4 +29,4 @@ $config['allow_any_cors_domain'] = TRUE;
 //Set all the allowable domains within the array
 //e.g. $config['allowed_origins'] =['http://www.example.com','https://spa.example.com']
 
-$config['allowed_cors_origins'] = ["https://esn.christianappdevelopers.com"];
+//$config['allowed_cors_origins'] = ["https://esn.christianappdevelopers.com"];

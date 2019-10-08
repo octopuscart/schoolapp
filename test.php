7cd3949a57fc33279586a92f69a2242663616330
dev <dev@develop.com>
2019-10-08 12:32:30 +0530

    changes

--- test.php
+++ test.php
@@ -2,5 +2,4 @@
 //error_reporting(E_ALL); 
 ini_set('display_errors', 0); 
 header("Access-Control-Allow-Origin: *"); 
-echo $filename = $_FILES['file']['name'];
-echo $img_type = $_FILES['file']['type'];
+print_r($_REQUEST);
